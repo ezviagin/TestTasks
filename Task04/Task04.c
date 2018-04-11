12e9e8c19d7fea6a9673573ec4dea35e019a269d
Egor Zviagintcev <ezvyagin@aquantia.com>
2018-04-11 23:52:09 +0300

    Async method changes (in progress, work not correctly)

--- Task04/Task04.c
+++ Task04/Task04.c
@@ -30,78 +30,90 @@ DWORD WINAPI AsyncCopyThread(LPVOID FilesToCopy)
         return -1;
     }
 
+    HANDLE hEventRead = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (hEventRead == NULL) {
+        printf("CreateEvent() failed. Code %d\n", GetLastError());
+        CloseHandle(hSource);
+        CloseHandle(hTarget);
+        return -1;
+    }
+    HANDLE hEventWrite = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (hEventWrite == NULL) {
+        printf("CreateEvent() failed. Code %d\n", GetLastError());
+        CloseHandle(hSource);
+        CloseHandle(hTarget);
+        CloseHandle(hEventRead);
+        return -1;
+    }
+
     UCHAR * buf = (UCHAR*)malloc(BUFF_SIZE);
     if (buf == NULL) {
         printf("Memory isn't allocated\n");
         CloseHandle(hSource);
         CloseHandle(hTarget);
+        CloseHandle(hEventRead);
+        CloseHandle(hEventWrite);
         return -1;
     }
     ZeroMemory(buf, BUFF_SIZE);
 
-    HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
-    if (hEvent == NULL) {
-        printf("CreateEvent() failed. Code %d\n", GetLastError());
-        CloseHandle(hSource);
-        CloseHandle(hTarget);
-        free(buf);
-        return -1;
-    }
+    OVERLAPPED OverlappedRead;
+    OVERLAPPED OverlappedWrite;
+   
+    ZeroMemory(&OverlappedRead, sizeof(OVERLAPPED));
+    ZeroMemory(&OverlappedWrite, sizeof(OVERLAPPED));
+   
+    OverlappedRead.hEvent  = hEventRead;
+    OverlappedWrite.hEvent = hEventWrite;
 
-    OVERLAPPED* Overlapped = (OVERLAPPED*)malloc(sizeof(OVERLAPPED));
-    if (Overlapped == NULL) {
-        printf("malloc(OVERLAPPED) failed, error %d\n", GetLastError());
-        CloseHandle(hSource);
-        CloseHandle(hTarget);
-        CloseHandle(hEvent);
-        free(buf);
-        return -1;
-    }
-    ZeroMemory(Overlapped, sizeof(OVERLAPPED));
-    Overlapped->hEvent = hEvent;
-
-    DWORD BytesWritten = 0;
-    DWORD BytesRead = 0;
+    LARGE_INTEGER BytesRead;
+    LARGE_INTEGER BytesWritten;
 
     INT RetValWrite = 0;
     INT RetValRead = 0;
     INT RetValOverlapped = 0;
 
+    UINT i = 0;
     while (TRUE)
     {
-        RetValRead = ReadFile(hSource, buf, BUFF_SIZE, NULL, Overlapped);
+        RetValRead = ReadFile(hSource, buf, BUFF_SIZE, NULL, &OverlappedRead);
         if (!RetValRead && (GetLastError() == ERROR_IO_PENDING)) {
-            RetValOverlapped = GetOverlappedResult(Overlapped->hEvent, Overlapped, &BytesRead, TRUE);
-            if (RetValOverlapped == FALSE) {
-                printf("AsyncCopyThread(): GetOverlappedResult() - EOF, error %d\n", GetLastError());
-                break;
-            }
-        }
-
-        RetValWrite = WriteFile(hTarget, buf, BUFF_SIZE, NULL, Overlapped);
-        if (!RetValWrite && (GetLastError() == ERROR_IO_PENDING)) {
-            RetValOverlapped = GetOverlappedResult(Overlapped->hEvent, Overlapped, &BytesWritten, TRUE);
-            if (RetValOverlapped == FALSE) {
-                printf("AsyncCopyThread(): GetOverlappedResult() failed, error %d\n", GetLastError());
+            RetValOverlapped = GetOverlappedResult(&OverlappedRead.hEvent, &OverlappedRead, &BytesRead.u, TRUE);
+            if (GetLastError() == ERROR_HANDLE_EOF) {
+                printf("Copying is completed, error %d\n", GetLastError());
                 break;
             }
         }
         
-        /* For debug */
-        if (Overlapped->Offset > 4194313000)
-            printf("Overlapped->Offset = %lld\n", (LONGLONG)Overlapped->Offset);
+        RetValWrite = WriteFile(hTarget, buf, BUFF_SIZE, NULL, &OverlappedWrite);
+        if (!RetValWrite && (GetLastError() == ERROR_IO_PENDING)) {
+            RetValOverlapped = GetOverlappedResult(&OverlappedWrite.hEvent, &OverlappedWrite, &BytesWritten.u, TRUE);
+            if (GetLastError() == FALSE) {
+                printf("Copying is completed, error %d\n", GetLastError());
+                break;
+            }
+        }
+        
+        if (BytesRead.QuadPart == 4194313)
+            printf("Stop here, BytesRead.QuadPart == %lld, error %d\n", BytesRead.QuadPart, GetLastError());
 
-        Overlapped->Offset += (ULONG)Overlapped->InternalHigh;
+        OverlappedWrite.Offset += BytesRead.LowPart;
+        OverlappedWrite.OffsetHigh += BytesRead.HighPart;
+        
+        OverlappedRead.Offset += BytesWritten.LowPart;
+        OverlappedRead.OffsetHigh += BytesWritten.HighPart;
+
+        ++i;
     }
 
     free(buf);
-    free(Overlapped);
-    
-    CloseHandle(hEvent);
+
     CloseHandle(hSource);
     CloseHandle(hTarget);
+    CloseHandle(hEventRead);
+    CloseHandle(hEventWrite);
     
-    return Overlapped->Offset;;
+    return 0;
 }
 
 INT FileCopyEx(WCHAR* wFileOut, WCHAR* wFileIn)
@@ -159,10 +171,18 @@ INT wmain(INT argc, WCHAR** argv)
         return -1;
     }
 
-    INT ReturnValue = FileCopyEx(argv[2], argv[1]);
-    /* Future return checks... */
+    HANDLE hEventMain = CreateEvent(NULL, FALSE, FALSE, NULL);
+    DWORD WaitMain = WaitForSingleObject(hEventMain, 0);
+    if (WaitMain == WAIT_FAILED) {
+        printf("Main(): WaitForSingleObject() failed, error %d\n", GetLastError());
+    }
 
-    printf("File \"%ls\" is copied to \"%ls\"\n", argv[1], argv[2]);
+
+    INT ReturnValue = FileCopyEx(argv[2], argv[1]);
+    if (ReturnValue == -1)
+        printf("File \"%ls\" is copied to \"%ls\"\n", argv[1], argv[2]);
+
+    SetEvent(hEventMain);
 
     _getch();
     return 0;
